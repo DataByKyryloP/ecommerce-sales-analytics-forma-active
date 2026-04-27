15d1b26c2ec37d2f11cf196234c18042ed9d2e97
DataByKyryloP <vikabezbdzika@gmail.com>
2026-04-27 13:37:25 +0300

    Update and rename 06_revenue_concentration.sql.txt to 06_revenue_concentration.sql

--- sql/06_revenue_concentration.sql
+++ sql/06_revenue_concentration.sql
@@ -1,28 +1,3 @@
--- Failed querySELECT
---         product_id,
---         SUM(price * quantity) AS revenue
---     FROM order_line_items
---     GROUP BY product_id,
---              ranked_products AS (
---     SELECT
---         product_id,:
--- 
---         revenue,
---         SUM(revenue) OVER (ORDER BY revenue DESC) AS cumulative_revenue,
---         SUM(revenue) OVER () AS total_revenue
---     FROM product_revenue
--- ),
--- 
--- final AS (
---     SELECT
---         product_id,
---         revenue,
---         cumulative_revenue / total_revenue AS revenue_ratio,
---         CASE
---             WHEN cumulative_revenue / total_revenue <= 0.8 THEN 'Top 80%'
---             ELSE 'Bottom 20%'
---         END AS pareto_flag
---     FROM ranked_products
 WITH product_revenue AS (
     SELECT
         product_id,
